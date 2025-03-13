9a69bd651dd8df1dae0e9ce8a7da2d8c5de8e8f7
AntoanetaStoyanova <antoanetapstoyanova@gmail.com>
2025-03-13 17:32:52 +0100

    test yml

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,28 @@
+# import pytest
+# from flask import Flask
+# from app import main_bp  # Assure-toi d'importer ton blueprint correctement
+
+# @pytest.fixture
+# def client():
+#     app = Flask(__name__)
+#     app.register_blueprint(main_bp)  # Associe ton blueprint ici
+#     app.config['TESTING'] = True  # Active le mode test
+#     client = app.test_client()
+#     yield client  # Le client est disponible pour les tests
+
+# # Test de la route d'accueil
+# def test_acceuil(client, mocker):
+#     # Mock de render_template
+#     mocker.patch('app.routes.render_template', return_value='Some HTML content')
+
+#     # Effectuer la requête GET
+#     response = client.get('/')
+
+#     # Vérification que le code de statut est 200
+#     assert response.status_code == 200
+#     # Vérification que le contenu retourné est celui du mock
+#     assert response.data == b'Some HTML content'
+
 import pytest
 from flask import Flask
 from app import main_bp  # Assure-toi d'importer ton blueprint correctement
@@ -6,7 +31,12 @@ from app import main_bp  # Assure-toi d'importer ton blueprint correctement
 def client():
     app = Flask(__name__)
     app.register_blueprint(main_bp)  # Associe ton blueprint ici
+    
+    # Utiliser une base de données en mémoire pour les tests
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Désactiver les notifications inutiles de modifications
     app.config['TESTING'] = True  # Active le mode test
+
     client = app.test_client()
     yield client  # Le client est disponible pour les tests
 
