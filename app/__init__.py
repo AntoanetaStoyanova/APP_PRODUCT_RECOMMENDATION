33085710fb81a11a914eb82fdada645cd8455e4b
AntoanetaStoyanova <antoanetapstoyanova@gmail.com>
2025-03-13 17:17:13 +0100

    test yml

--- app/__init__.py
+++ app/__init__.py
@@ -111,7 +111,7 @@ def load_user(user_id):
 def create_app():
     app = Flask(__name__)
 
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRESQL_URI')
     app.config.from_object(Config)
     
 
