481f18b4bf2e24eef1382d41278ffc20e07f95af
AntoanetaStoyanova <antoanetapstoyanova@gmail.com>
2025-03-28 11:35:33 +0100

    new embedding model

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,3 +1,25 @@
-from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_openai import AzureOpenAIEmbeddings
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+import sys
+import os
+
+# Ajouter le dossier parent au chemin pour pouvoir importer config.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from config import Config
+
+
+
+
+
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+embeddings = AzureOpenAIEmbeddings(openai_api_key=Config.AZURE_OPENAI_API_KEY,
+                                    azure_deployment='text-embedding-3-large',
+                                    azure_endpoint=Config.AZURE_OPENAI_ENDPOINT,
+                                    openai_api_version="2023-05-15",
+                                    chunk_size=500
+)
--- app/prompt.py
+++ app/prompt.py
@@ -2,9 +2,23 @@
 
 from langchain.prompts import ChatPromptTemplate
 
+# prompt_template = """
+# Vous êtes un assistant pour des tâches de recommandation de produits.
+# Utilisez les informations suivantes sur les produits pour répondre à la question de l'utilisateur.
+# Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas.
+
+# Question : {question}
+
+# Contexte (produits) : {context}
+
+# Réponse : 
+# """
+
+
 prompt_template = """
 Vous êtes un assistant pour des tâches de recommandation de produits.
-Utilisez les informations suivantes sur les produits pour répondre à la question de l'utilisateur.
+Utilisez les informations suivantes sur les produits pour répondre à la question de l'utilisateur, 
+en lui donnant des informations interessentes sur les trois produits recommandés. Il ne faut pas lui montrer le url. 
 Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas.
 
 Question : {question}
