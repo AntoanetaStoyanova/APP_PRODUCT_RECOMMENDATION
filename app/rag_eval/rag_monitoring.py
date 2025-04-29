bde24ea3e86ad12e7915a19b766fe0b28a3699a4
AntoanetaStoyanova <antoanetapstoyanova@gmail.com>
2025-04-29 17:28:29 +0200

    Model metric

--- app/rag_eval/rag_monitoring.py
+++ app/rag_eval/rag_monitoring.py
@@ -3,9 +3,33 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import os
+from langchain_openai import AzureOpenAIEmbeddings
+
+import sys
+
+
+# Ajouter le dossier parent au chemin pour pouvoir importer config.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+from config import Config
+
+
+
+
+
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+embeddings = AzureOpenAIEmbeddings(
+    openai_api_key=Config.AZURE_OPENAI_API_KEY,
+    azure_deployment='text-embedding-3-large',
+    azure_endpoint=Config.AZURE_OPENAI_ENDPOINT,
+    openai_api_version="2023-05-15",
+    chunk_size=500
+)
 
 # Charger le modèle SentenceTransformer
-model = SentenceTransformer('all-MiniLM-L6-v2')
+# model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Charger les données
 df = pd.read_csv("app/recommendations.csv")
@@ -26,12 +50,15 @@ for query, response, source in zip(queries, responses, sources):
     start_time = time.time()  
 
     # Encodage des textes
-    embeddings = model.encode([query, response, source])
+    # embeddings = embeddings.encode([query, response, source])
+    embedding_vectors = embeddings.embed_documents([query, response, source])
 
     # Calcul des similarités
-    similarity_query_response = cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
-    similarity_response_source = cosine_similarity([embeddings[1]], [embeddings[2]])[0][0]
-
+    # similarity_query_response = cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
+    # similarity_response_source = cosine_similarity([embeddings[1]], [embeddings[2]])[0][0]
+    similarity_query_response = cosine_similarity([embedding_vectors[0]], [embedding_vectors[1]])[0][0]
+    similarity_response_source = cosine_similarity([embedding_vectors[1]], [embedding_vectors[2]])[0][0]
+    
     end_time = time.time()  
     latency = (end_time - start_time) * 1000  
 
